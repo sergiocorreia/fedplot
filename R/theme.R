da9973fbd0ca37021184c6eab63b564f615fae07
Sergio Correia <sergio.correia@gmail.com>
2023-06-05 08:45:03 -0400

    Update theme.R

--- R/theme.R
+++ R/theme.R
@@ -14,7 +14,7 @@
 #' Alternative to ggplot::.pt ; using bigpoints of size 72 instead of points of size 72.27.
 #'
 #' @export
-#' @keyword internal
+#' @keywords internal
 .bigpt <- 72 / 25.4 # Alternative to ggplot::.pt === (72.27 / 25.4)
 
 
